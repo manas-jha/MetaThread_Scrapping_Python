772ebf9bd1612ed15a5e721ebfbb317eaf046907
Manas Kumar <kumarmanas2002@gmail.com>
2023-12-19 14:28:41 +0530

    Working all good

--- quickstart.py
+++ quickstart.py
@@ -9,20 +9,47 @@ def main():
     username = None
     password = None
     if not all([username, password]):
-        username = str(input("Enter Your Username or Email :"))
-        password = str(input("Enter Your Password :"))
+        # username = str(input("Enter Your Username or Email :"))
+        # password = str(input("Enter Your Password :"))
+        username = "jyoti.pandey.1"
+        password = "Manas@123"
     threads.login(username, password)
 
+
+
     # check logged in user
-    print(f"Logged In User {threads.me}")
+    # print(f"Logged In User {threads.me}")
+    print()
+    print()
+    print()
+
+
 
     # get thread details
-    print(threads.get_thread(3138977881796614961))
-    print()
-    print()
-    print()
-    print()
-    print(threads.get_thread("https://www.threads.net/@zuck/post/CuP48CiS5sx"))
+    # print(threads.get_thread("https://www.threads.net/@zuck/post/CuP48CiS5sx"))
+
+    # store_thread = threads.get_thread("https://www.threads.net/@zuck/post/CuP48CiS5sx") #Mark Zukerberg
+    store_thread = threads.get_thread("https://www.threads.net/@fully_fitness_tips/post/C1BFCp4S33K")
+    print(type(store_thread))
+    print(store_thread)
+
+    
+
+    # try understanding data hirarchy
+    containing_element = store_thread["containing_thread"]["thread_items"][0]["post"]['text_post_app_info']
+    
+    for key, value in containing_element.items():
+        print(key, value)
+
+    # for key, value in store_thread.items():
+        # print(key)
+        # print(key, value)
+
+
+
+
+
+
     #threads.get_thread("thread_id or thread_url")
     """
     Here is an example
